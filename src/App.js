8e344ef64aa7baecd7dacfe03e44cfa461b9b655
Alex Shan <shantong.sir@gmail.com>
2022-12-08 23:14:36 +0800

    feat: add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import routesConfig from "@/routesConfig";
 import { useRoutes } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  return useRoutes(routesConfig);
+  return (
+    <>
+      <Header />
+      {useRoutes(routesConfig)}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header(params) {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/counter">Counter</Link>
+      </li>
+    </ul>
+  );
+}
