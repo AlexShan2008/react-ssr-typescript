fd1b239dd3444bb69766287faaa8bd571b357fcd
Alex Shan <shantong.sir@gmail.com>
2022-12-09 21:09:17 +0800

    feat: install `source-map-loader` and `axios`

--- webpack-config-base.js
+++ webpack-config-base.js
@@ -11,6 +11,11 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.js$/,
         use: {
           loader: "babel-loader",
           options: {
