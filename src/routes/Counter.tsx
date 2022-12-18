0c08bfaf1ceea58697ebb54542262838d098a782
Alex Shan <shantong.sir@gmail.com>
2022-12-18 22:27:15 +0800

    feat: remove unused ESLint codes

--- src/routes/Counter.tsx
+++ src/routes/Counter.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import actionCreators from "@/store/actionCreators/counter";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function Counter() {
   const dispatch = useDispatch();
+  const [age, setAge] = useState(0);
   const number = useSelector((state) => state.counter.number);
+
+  useEffect(() => {
+    setAge(0);
+  });
 
   return (
     <div>
